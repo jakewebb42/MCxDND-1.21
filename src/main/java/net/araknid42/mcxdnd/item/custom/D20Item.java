6b085563ce6f6a030d316c53e48203852f9c0425
jakewebb42 <jakewebb36@gmail.com>
2025-03-30 15:18:58 -0500

    3_30_2 D20 rolling with output on action bar

--- src/main/java/net/araknid42/mcxdnd/item/custom/D20Item.java
+++ src/main/java/net/araknid42/mcxdnd/item/custom/D20Item.java
@@ -1,48 +1,59 @@
 package net.araknid42.mcxdnd.item.custom;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.Style;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.FireworkRocketEntity;
+import net.minecraft.world.item.*;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
+import java.util.List;
 import java.util.Map;
 
 public class D20Item extends Item {
-    private static final Map<Block, Block> D20_MAP =
-            Map.of(
-              Blocks.STONE, Blocks.STONE_BRICKS,
-              Blocks.END_STONE, Blocks.END_STONE_BRICKS
-            );
 
     public D20Item(Properties pProperties) {
         super(pProperties);
     }
 
 
+
     @Override
-    public InteractionResult useOn(UseOnContext pContext) {
-        Level level = pContext.getLevel();
-        Block clickedBlock = level.getBlockState(pContext.getClickedPos()).getBlock();
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
 
-        if(D20_MAP.containsKey(clickedBlock)) {
-            if(!level.isClientSide()) {
-                level.setBlockAndUpdate(pContext.getClickedPos(), D20_MAP.get(clickedBlock).defaultBlockState());
+        // Init
+        ItemStack itemstack = pPlayer.getItemInHand(pHand);
+        BlockPos pos = new BlockPos((int)pPlayer.getX(), (int)pPlayer.getY(), (int)pPlayer.getZ());
 
-                pContext.getItemInHand().hurtAndBreak(0, ((ServerLevel) level), ((ServerPlayer) pContext.getPlayer()),
-                        item -> pContext.getPlayer().onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+        // Roll
+        int roll = (int)(Math.random()*20 + 1);
+        String rollString = String.valueOf(roll);
 
-                level.playSound(null,pContext.getClickedPos(), SoundEvents.GRINDSTONE_USE, SoundSource.BLOCKS);
-            }
+        // Output Roll
+        if (!pLevel.isClientSide) {
+            pPlayer.displayClientMessage(Component.literal(rollString), true);
+            pLevel.playSound(null, pos, SoundEvents.FIREWORK_ROCKET_SHOOT, SoundSource.BLOCKS);
         }
 
-        return InteractionResult.SUCCESS;
+        return InteractionResultHolder.sidedSuccess(pPlayer.getItemInHand(pHand), pLevel.isClientSide());
     }
+
 }
